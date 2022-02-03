0e100df170284a332f982626111cbf0f74c7ffb7
Jim Hofman <james.e.hofman@jpl.nasa.gov>
2022-02-03 09:26:34 -0800

    #41 HLS name parsing - first pass

---
+++ src/opera/util/img_utils.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+#
+# Copyright 2021, by the California Institute of Technology.
+# ALL RIGHTS RESERVED.
+# United States Government sponsorship acknowledged.
+# Any commercial use must be negotiated with the Office of Technology Transfer
+# at the California Institute of Technology.
+# This software may be subject to U.S. export control laws and regulations.
+# By accepting this document, the user agrees to comply with all applicable
+# U.S. export laws and regulations. User has the responsibility to obtain
+# export licenses, or other export authority as may be required, before
+# exporting such information to foreign countries or providing access to
+# foreign persons.
+#
+
+"""
+=================
+image_utils.py
+=================
+
+"""
+
+from collections import namedtuple
+from datetime import datetime
+
+
+def get_dwsx_hls_filename_fields(file_name, julian=True):
+
+    Fields = namedtuple('Fields', ['product', 'short_name', 'MGRS_tile_id', 'time_of_acquisition',
+                                   'collection_version', 'sub_version', 'band_or_QA_fmask', 'data_format'])
+    fields = Fields._make(file_name.split('.'))._asdict()
+
+    if julian:
+        julian_time = fields['time_of_acquisition'].split('T')
+        julian_time[0] = str(datetime.strptime(julian_time[0], '%Y%j').date()).replace('-', '')
+        fields['time_of_acquisition'] = 'T'.join(julian_time)
+
+    return fields
+
+
+if __name__ == '__main__':
+    x = get_dwsx_hls_filename_fields("HLS.S30.T53SMS.2020276T013701.v1.5.B01.tif")
+    print(x)
